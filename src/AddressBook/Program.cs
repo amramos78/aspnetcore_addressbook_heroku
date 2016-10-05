17a36f85bcceba78df0a2737b87c8691d6fbe5b9
Alejandro R <Alejandro R>
2016-10-05 10:03:27 -0300

    update

--- src/AddressBook/Program.cs
+++ src/AddressBook/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
+using Microsoft.Extensions.Configuration;
+
 namespace AddressBook
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
             var host = new WebHostBuilder()
             .UseKestrel()
             .UseContentRoot(Directory.GetCurrentDirectory())
